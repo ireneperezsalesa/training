b86a5e16075bb7a1c67b487df1571cb40ababdb9
ireneperezsalesa <ireneperezsalesa@gmail.com>
2021-01-27 11:44:04 +0100

    Updates: changes in Dataset (padding) and state updates.

--- training.py
+++ training.py
@@ -3,6 +3,7 @@ import lpips
 import torchvision
 import os
 import time
+import math
 from model.model import *
 from model.unet import *
 from model.submodules import *
@@ -41,21 +42,35 @@ for t in range(160):  # TRAIN FOR 160 EPOCHS
     for seq in sorted(os.listdir(root)):
         print('Training ' + seq)
 
-        # Create dataset for the sequence
-        dataset = EventData(root=root, seq=seq, event_dir=event_dir, frame_dir=frame_dir)
-        # Load sequence in batches of 2
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=2) 
+        try:
+            # Create dataset for the sequence
+            dataset = EventData(root=root, seq=seq, event_dir=event_dir, frame_dir=frame_dir)
+        except:
+            print('Error creating dataset at epoch ', t, 'seq', seq)
 
-        prev_states=None
+        try: 
+            # Load sequence in batches of 2
+            dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=False) 
+        except:
+            print('Error loading dataset at epoch ', t, 'seq', seq)
+
+        # Initialize states
+        states=None
 
         for i, data in enumerate(dataloader, 0):
             x, y = data[0].to(device), data[1].to(device)
 
-            # Forward pass: compute predicted y by passing x to the model.
-            y_pred, states = model.forward(x, prev_states)
+            try:
+                # Forward pass: compute predicted y by passing x to the model.
+                y_pred, states = model.forward(x, states)
+            except:
+                print('Error in forward pass at epoch ', t, 'seq', seq)
 
-            # Compute and print loss.
-            loss = loss_fn_vgg(y_pred, y)
+            try:
+                # Compute and print loss.
+                loss = loss_fn_vgg(y_pred, y)
+            except:
+                print('Error computing loss at epoch ', t, 'seq', seq)
 
             # Before the backward pass, use the optimizer object to zero all of the
             # gradients for the variables it will update.
@@ -70,12 +85,26 @@ for t in range(160):  # TRAIN FOR 160 EPOCHS
             optimizer.step()
 
             with torch.no_grad():
-                prev_states = states
+
+                # Save model, checkpoint several times per epoch
+                if seq.endswith('49'):
+                    print('Saving')
+                    time_now = time.time()
+                    train_time = time_now - start
+
+                    cp_file_name = '/home/lab101m5/event/rpg_e2vid/checkpoints/cp_' + str(t) + seq + '.tar'
+                    model_name = '/home/lab101m5/event/rpg_e2vid/checkpoints/m_' + str(t) + seq + '.pth'
+
+                    torch.save({
+                        'last_epoch': t,
+                        'last_seq': seq,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'loss': loss,
+                        'train_time': train_time
+                    }, cp_file_name)
+                    torch.save(model, model_name) 
 
 # Total training time
 end = time.time()
 print('Training time: ', end-start, ' s')
-
-# GPU tensor to CPU, save model
-model = model.cpu()
-#torch.save(model, PATH) #'/home/lab101m5/event/rpg_e2vid/trained_model.pth') 
